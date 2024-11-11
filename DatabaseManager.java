5bff87b35cac9c90fa1e03e8c55326362e6677fd
Nicola1503 <122787040+Nicola1503@users.noreply.github.com>
2024-11-11 20:33:39 +0100

    Update DatabaseManager.java

--- DatabaseManager.java
+++ DatabaseManager.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DatabaseManager {
     private static final String URL = "jdbc:mysql://localhost:3306/securenotesdb";
     private static final String USER = "root";
-    private static final String PASSWORD = "xY00zH19qX!o";
+    private static final String PASSWORD = "password";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
